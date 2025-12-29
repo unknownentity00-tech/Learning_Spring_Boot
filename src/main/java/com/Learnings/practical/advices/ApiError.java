0cca2e449b680e753f7a70ae995a2e8a10f8f03e
DEBANGSHU MANDAL <debangshumandal@DEBANGSHUs-MacBook-Air.local>
2025-12-29 20:56:43 +0530

    created the  custon exception handler and errors

---
+++ src/main/java/com/Learnings/practical/advices/ApiError.java
@@ -0,0 +1,19 @@
+package com.Learnings.practical.advices;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+}
