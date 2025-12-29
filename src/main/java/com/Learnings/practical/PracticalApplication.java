ddff15742ed8834bf16d1a57ad2a6d22e115126d
DEBANGSHU MANDAL <debangshumandal@DEBANGSHUs-MacBook-Air.local>
2025-12-29 21:24:58 +0530

    1: built the apis  post, get , put , delete 2: maked the changes  to understand new things
    (cherry picked from commit 0739ca69ea9a257aaf03acba7d465884934b3218)

--- src/main/java/com/Learnings/practical/PracticalApplication.java
+++ src/main/java/com/Learnings/practical/PracticalApplication.java
@@ -1,9 +1,7 @@
 package com.Learnings.practical;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticalApplication {
@@ -11,9 +9,5 @@ public class PracticalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticalApplication.class, args);
 	}
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
 }
