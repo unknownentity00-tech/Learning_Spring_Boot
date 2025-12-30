0e13cf8912989481a8986646144bc0427245943d
DEBANGSHU MANDAL <debangshumandal@DEBANGSHUs-MacBook-Air.local>
2025-12-30 19:59:23 +0530

    learned the most basic autheticate

--- src/main/java/com/Learnings/practical/config/WebSecurityConfig.java
+++ src/main/java/com/Learnings/practical/config/WebSecurityConfig.java
@@ -12,9 +12,14 @@ public class WebSecurityConfig {
 @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
    httpSecurity
+           .authorizeHttpRequests(auth->auth
+                   .requestMatchers("/public /**").permitAll()
+                   .requestMatchers("/restraunt/**").authenticated()
+           )
            .formLogin(Customizer.withDefaults());
    return httpSecurity.build();
   }
-
+//here the main significance holds is permitAll() that means anyone can access authenticated()
+// means the login page will apear  as a auhteticator
 
 }
